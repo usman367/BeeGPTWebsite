b5697a09074541f7194e5d1e951442f1cd382113
Usman <usmansha367@gmail.com>
2024-02-11 19:18:31 +0000

    Integrated PlanetNet API

--- plant_identifier/views.py
+++ plant_identifier/views.py
@@ -1,10 +1,7 @@
-from urllib import request
-
 from django.shortcuts import render
-from django.template import context
-
 from .forms import PlantImageForm
-from .models import PlantImage
+import requests
+
 
 # Create your views here.
 def upload_image(request):
@@ -12,6 +9,20 @@ def upload_image(request):
         form = PlantImageForm(request.POST, request.FILES)
         if form.is_valid():
             plant_image = form.save()
+
+            # Call to PlanetNet API
+            api_url = 'https://my-api.plantnet.org/v2/identify/all'
+            api_key = '2b10kJ93KxHKQXXnSGKDxuRpfe'
+            files = {'images': plant_image.image}
+
+            response = requests.post(api_url, files=files, headers={'Authorization': f'Bearer {api_key}'})
+            if response.status_code == 200:
+                data = response.json()
+                # The API returns a field 'species' with the species name
+                common_name = data["results"][0]["species"]["commonNames"][0]
+                plant_image.species = common_name
+                plant_image.save()
+
             return render(request, 'image_upload/image_result.html', {'plant_image': plant_image})
     else:
         form = PlantImageForm()
