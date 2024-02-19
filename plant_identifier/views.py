a26bc1ca5b2c31eb7792f09701f6d0cfccaae4a7
Usman <usmansha367@gmail.com>
2024-02-19 12:46:34 +0000

    Added model prediction

--- plant_identifier/views.py
+++ plant_identifier/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from openai import OpenAI
 from .forms import PlantImageForm
 import requests
-
+from django.core.files.storage import FileSystemStorage
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
 
 # Create your views here.
 def upload_image(request):
@@ -19,6 +22,14 @@ def upload_image(request):
                 api_key="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"
             )
             plant_info = ""
+            model = load_model(
+                'C://Users//usman//PycharmProjects//image_classifier//classifier//models//plantsimageclassifier2.h5')
+            fs = FileSystemStorage()
+            uploaded_file = request.FILES['image']
+            filename = fs.save(uploaded_file.name, uploaded_file)
+            label_to_index = {'Bluebell': 0, 'Buttercup': 1, 'Coltsfoot': 2, 'Cowslip': 3, 'Crocus': 4, 'Daffodil': 5,
+                              'Daisy': 6, 'Dandelion': 7, 'Fritillary': 8, 'Iris': 9, 'Lily Valley': 10, 'Pansy': 11,
+                              'Snowdrop': 12, 'Sunflower': 13, 'Tigerlily': 14, 'Tulip': 15, 'Windflower': 16}
 
             response = requests.post(api_url, files=files, headers={'Authorization': f'Bearer {api_key}'})
             if response.status_code == 200:
@@ -27,6 +38,13 @@ def upload_image(request):
                 common_name = data["results"][0]["species"]["commonNames"][0]
                 plant_image.species = common_name
                 plant_image.save()
+
+                # Preprocess the image and predict
+                processed_image = preprocess_image(fs.path(filename))
+                prediction = model.predict(processed_image)
+                predicted_label = np.argmax(prediction, axis=1)
+                class_labels = list(label_to_index.keys())  # Get class labels from the label-to-index mapping
+                predicted_class = class_labels[predicted_label[0]]
 
                 try:
                     prompt = f"Provide information about the biodiversity and ecosystem of the {plant_image.species}."
@@ -44,7 +62,8 @@ def upload_image(request):
 
             return render(request, 'image_upload/image_result.html', {
                 'plant_image': plant_image,
-                'plant_info': plant_info
+                'plant_info': plant_info,
+                'predicted_class': predicted_class
             })
     else:
         form = PlantImageForm()
@@ -58,3 +77,11 @@ def biodiversity(request):
 
 def tutorial(request):
     return render(request, 'image_upload/tutorial.html')
+
+
+def preprocess_image(img_path, target_size=(256, 256)):
+    img = image.load_img(img_path, target_size=target_size)
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return img_array_expanded_dims
