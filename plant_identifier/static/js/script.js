f89f440f2a86a87230a01840b9aeeaaa2f0b8988
Usman <usmansha367@gmail.com>
2024-04-01 17:04:34 +0100

    Added a image slideshow

--- plant_identifier/static/js/script.js
+++ plant_identifier/static/js/script.js
@@ -1,6 +1,5 @@
-// Utilised briancodex's public GitHub repository for a responsive navbar
-// https://github.com/briancodex/html-css-js-website-smooth-scroll
-
+// Utilised briancodex's public GitHub repository (last accessed: 31/03/2024) for a responsive navbar
+// Available at: https://github.com/briancodex/html-css-js-website-smooth-scroll
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
 const navLogo = document.querySelector('#navbar-title');
@@ -26,3 +25,28 @@ const hideMobileMenu = () => {
 
   menuLinks.addEventListener('click', hideMobileMenu);
   navLogo.addEventListener('click', hideMobileMenu);
+
+
+
+// For the image slideshow on the home page
+// Utilised this example (last accessed: 31/03/2024): https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    slides[i].style.opacity = "0";
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(function() { slides[slideIndex-1].style.opacity = "1"; }, 100); // Start fade in effect
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
